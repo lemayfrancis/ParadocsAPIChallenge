310d9be7baaf3653edf0845901d8b955a69719e2
Francis L <lemayfrancis@outlook.com>
2020-03-14 11:01:43 -0400

    Initialize project with Spring Initializr

---
+++ ParadocsAPIChallenge/src/test/java/com/lemayfrancis/ParadocsAPIChallenge/ParadocsApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemayfrancis.ParadocsAPIChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadocsApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
